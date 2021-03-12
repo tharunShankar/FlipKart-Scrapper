66cdff4480889c828baae78f710ba86469d2b391
Kavita Maurya <kavita.maurya@capgemini.com>
2021-03-12 22:57:54 +0530

    Commit done

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 # doing necessary imports
 import threading
+import io
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
 from logger_class import getLog
 from flask import Flask, render_template, request, jsonify, Response, url_for, redirect
 from flask_cors import CORS, cross_origin
@@ -8,7 +12,6 @@ from mongoDBOperations import MongoDBManagement
 from FlipkratScrapping import FlipkratScrapper
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-import json
 
 rows = {}
 collection_name = None
@@ -16,15 +19,17 @@ collection_name = None
 logger = getLog('flipkrat.py')
 
 free_status = True
+db_name = 'Flipkart-Scrapper'
 
 app = Flask(__name__)  # initialising the flask app with the name 'app'
 
+#For selenium driver implementation on heroku
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument("disable-dev-shm-usage")
 
-
+#To avoid the time out issue on heroku
 class threadClass:
 
     def __init__(self, expected_review, searchString, scrapper_object, review_count):
@@ -52,6 +57,7 @@ class threadClass:
 def index():
     if request.method == 'POST':
         global free_status
+        ## To maintain the internal server issue on heroku
         if free_status != True:
             return "This website is executing some process. Kindly try after some time..."
         else:
@@ -63,19 +69,19 @@ def index():
             scrapper_object = FlipkratScrapper(executable_path=ChromeDriverManager().install(),
                                                chrome_options=chrome_options)
             mongoClient = MongoDBManagement(username='Kavita', password='kavita1610')
-            db_name = 'Flipkart-Scrapper'
             scrapper_object.openUrl("https://www.flipkart.com/")
             logger.info("Url hitted")
             scrapper_object.login_popup_handle()
             logger.info("login popup handled")
             scrapper_object.searchProduct(searchString=searchString)
-            logger.info("Search begins")
+            logger.info(f"Search begins for {searchString}")
             if mongoClient.isCollectionPresent(collection_name=searchString, db_name=db_name):
                 response = mongoClient.findAllRecords(db_name=db_name, collection_name=searchString)
                 reviews = [i for i in response]
                 if len(reviews) > expected_review:
                     result = [reviews[i] for i in range(0, expected_review)]
-                    scrapper_object.saveDataFrameToFile(file_name="static/scrapper_data.csv", dataframe=pd.DataFrame(result))
+                    scrapper_object.saveDataFrameToFile(file_name="static/scrapper_data.csv",
+                                                        dataframe=pd.DataFrame(result))
                     logger.info("Data saved in scrapper file")
                     return render_template('results.html', rows=result)  # show the results to user
                 else:
@@ -101,7 +107,6 @@ def index():
 def feedback():
     try:
         global collection_name
-        print(collection_name)
         if collection_name is not None:
             scrapper_object = FlipkratScrapper(executable_path=ChromeDriverManager().install(),
                                                chrome_options=chrome_options)
@@ -117,6 +122,30 @@ def feedback():
     except Exception as e:
         raise Exception("(feedback) - Something went wrong on retrieving feedback.\n" + str(e))
 
+
+@app.route("/graph", methods=['GET'])
+@cross_origin()
+def graph():
+    return redirect(url_for('plot_png'))
+
+
+@app.route('/a', methods=['GET'])
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+
+def create_figure():
+    data = pd.read_csv("static/scrapper_data.csv")
+    dataframe = pd.DataFrame(data=data)
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = dataframe['product_searched']
+    ys = dataframe['rating']
+    axis.scatter(xs, ys)
+    return fig
 
 if __name__ == "__main__":
     app.run()  # running the app on the local machine on port 8000
